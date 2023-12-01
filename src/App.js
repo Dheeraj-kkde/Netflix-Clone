4b771c22a34a52397d40c20525acddb4db26e099
Dheeraj Kumar <91405138+Dheeraj-kkde@users.noreply.github.com>
2023-12-01 19:46:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        Basic skeleton to build the react application similar to NETFLIX
+    </div>
+  );
+}
+
+export default App;
