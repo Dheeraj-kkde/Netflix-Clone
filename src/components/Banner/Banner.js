4a82abdc0d26834ce13e5e3cd0b261615f80b98f
Dheeraj Kumar <91405138+Dheeraj-kkde@users.noreply.github.com>
2023-12-02 15:02:12 +0530

    fix the issue

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import instance from "../../axios";
+import requests from "../../requests";
+import "./Banner.css";
+
+const base_image_url = "https://image.tmdb.org/t/p/original/";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await instance.get(requests.fetchNetflixOriginals);
+      const response = await request.data.results;
+      const randomMovie =
+        response[Math.floor(Math.random() * response.length - 1)];
+      setMovie(randomMovie);
+    }
+    fetchData();
+  }, []);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substring(0, n - 1) + "..." : str;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(${base_image_url}${movie?.backdrop_path})`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
+      </div>
+      <div className="banner_fadeBottom" />
+    </header>
+  );
+}
+
+export default Banner;
